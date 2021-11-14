804d13a4c8f018ba0558b4f99a3b7f42c5a56e58
money-dog <1983519622@qq.com>
2021-11-14 20:46:14 +0800

    '完成了登录和注册功能的开发'

---
+++ js/login.js
@@ -0,0 +1,76 @@
+$(function () {
+  // 点击注册账号的链接
+  $("#link_reg").on("click", function () {
+    $(".login-box").hide();
+    $(".reg-box").show();
+  });
+
+  // 点击登录的链接
+  $("#link_login").on("click", function () {
+    $(".login-box").show();
+    $(".reg-box").hide();
+  });
+
+  // 从layui获取form对象，自定义校验规则
+  let form = layui.form;
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+    ],
+    username: function (value, item) { //value：表单的值、item：表单的DOM对象
+      if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+        return '用户名不能有特殊字符';
+      }
+      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+        return '用户名首尾不能出现下划线\'_\'';
+      }
+      if (/^\d+\d+\d$/.test(value)) {
+        return '用户名不能全为数字';
+      }
+    },
+    repwd: function (value) {
+      let pwd = $('.reg-box [name=password]').val();
+      if (pwd != value) {
+        return '两次密码不一致';
+      }
+    }
+  })
+
+  // 监听注册表单提交事件
+  let layer = layui.layer;
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/reguser",
+      data: {
+        username: $('#form_reg [name=account]'),
+        password: $('#form_reg [name=password]'),
+      },
+      success: function (res) {
+        if (res.status != 0) {
+          return layer.msg('只想弱弱提示');
+        } else {
+          console.log('success')
+        }
+      }
+    });
+  });
+
+  $('#form_login').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/login",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status != 0) {
+          return layer.msg('只想弱弱提示');
+        } else {
+          location.href = '../index.html';
+          layer.msg('成功');
+        }
+      }
+    });
+  })
+});
